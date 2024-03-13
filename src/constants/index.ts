9e40f0d464388c178aef7c3f59fca5e51b540ae2
ShehrozMalik12 <s.malik@ssidecisions.com>
2024-03-13 23:43:19 +0500

    Increased page size + changed app title

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const SEED = "REACT-TASK";
-export const PAGE_SIZE = "10";
+export const PAGE_SIZE = "18";
 export const PAGE_COUNT = 10;
 export const BASE_URL = "https://randomuser.me/api/";
 export const MAPBOX_ACCESS_TOKEN =
